ff8d9eb709ebec6ce4f903ff418db8a68ea83317
AlonsoAliaga <36780081+AlonsoAliaga@users.noreply.github.com>
2025-05-08 21:09:07 -0500

    Updated index.html

--- docs/script.js
+++ docs/script.js
@@ -29,7 +29,7 @@ function checkSite(window) {
     check()
     setInterval(()=>{
         check()
-    },1000 * 2);
+    },1000 * 60);
     setTimeout(()=>{
       let href = window.location.href;
       if(!href.includes(atob("YWxvbnNvYWxpYWdhLmdpdGh1Yi5pbw=="))) {
@@ -86,35 +86,80 @@ function loadCounter() {
  }
 }
 function check(lockAfter) {
-    let a = atob("aHR0cHM6Ly9hcGkuZ2l0aHViLmNvbS9yZXBvcy9BbG9uc29BbGlhZ2EvQWxvbnNvQWxpYWdhQVBJL2NvbnRlbnRzL2FwaS90YWdzL2d1aWxkLXRhZ3MuanNvbj9yZWY9bWFpbg==");
-    fetch(a)
+    //let a = atob("aHR0cHM6Ly9hcGkuZ2l0aHViLmNvbS9yZXBvcy9BbG9uc29BbGlhZ2EvQWxvbnNvQWxpYWdhQVBJL2NvbnRlbnRzL2FwaS90YWdzL2d1aWxkLXRhZ3MuanNvbj9yZWY9bWFpbg==");
+    let a = atob("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0Fsb25zb0FsaWFnYS9BbG9uc29BbGlhZ2FBUEkvcmVmcy9oZWFkcy9tYWluL2FwaS90YWdzL2d1aWxkLXRhZ3MuanNvbg==");
+    var myHeaders = new Headers();
+    myHeaders.append('Content-Type','text/plain; charset=UTF-8');
+    fetch(a,myHeaders)
       .then(res => res.json())
       .then(content => {
+        return;
+        console.log("START")
         const decoded = atob(content.content);
+        //console.log(decoded)
         const parsed = JSON.parse(decoded);
+        //console.log(parsed)
         update(parsed,lockAfter);
+    }).catch(e=>{
+        console.log(`Error content:`);
+        console.log(e);
+    });
+    axios.get(`${a}`)
+      .then(function (response) {
+        console.log('Response data:', response);
+        console.log("START")
+        console.log(response.data);
+        const decoded = response.data;
+        /*
+        if(true){
+          let a = document.createElement("div");
+          a.textContent = he.decode(decoded);
+          let b = document.createElement("div");
+          b.innerHTML = he.decode(decoded);
+          let c = document.createElement("div");
+          c.innerText = he.decode(decoded);
+          let d = document.createElement("div");
+          d.value = he.decode(decoded);
+          document.body.appendChild(a);
+          document.body.appendChild(b);
+          document.body.appendChild(c);
+          document.body.appendChild(d);
+        }
+        console.log(decoded)
+        const parsed = JSON.parse(decoded);
+        console.log(parsed)
+        */
+        update(decoded,lockAfter);
     }).catch(e=>{
         console.log(`Error content:`);
         console.log(e);
     });
 }
 let stored = undefined;
+let newHeight = undefined;
 function update(data,lockAfter) {
     const container = document.getElementById('guild-tags');
     if (data.length > 0) {
         let html = '<table style="margin: 0 auto;"><thead><tr><th>Guild Tag</th><th>Members Amount</th><th>Guild Name</th><th>Invitation</th></tr></thead><tbody>';
         data.forEach(item => {
+          //console.log(item.alt);
+          //console.log(he.encode(item.alt));
+          if(false){
+            let a = document.createElement("div");
+            a.innerHTML = he.encode(item.alt);
+            document.body.appendChild(a);
+          }
           if(item.lName && item.lName.includes("Invite") && !item.lName.includes(" Invite")) {
             item.lName = "🔗 Invite";
           }
-          tagName = item.alt == "IOS" ? "iOS" : item.alt;
+          tagName = item.tag == "IOS" ? "iOS" : item.tag;
           let show = `<span style="display:inline-block; padding:4px 8px; background-color:#545454; color:#fff; font-size:16px; font-weight:bold; border-radius:4px; text-transform:uppercase;font-family: Arial;"><img src="${item.img}" alt="${item.alt}" width="20"> ${tagName}</span>`
           html += `
             <tr>
               <td>${show}${getTinyTag(item.nStatus,item.members)}</td>
               <td>${item.members}</td>
-              <td>${item.name.replace(/[^a-zA-Z0-9.-_&\s+]/g,"")}</td>
-              <td><a href="${item.invite}" target="_blank">${(item.lName || "🔗 Invite").replace(/ð/g,'🔗')}</a></td>
+              <td>${item.name/*.replace(/[^a-zA-Z0-9.-_&\s+]/g,"")*/}</td>
+              <td><a href="${item.invite}" target="_blank">${(item.lName || "🔗 Invite")/*.replace(/ð/g,'🔗')*/}</a></td>
             </tr>
           `;
         });
@@ -122,6 +167,10 @@ function update(data,lockAfter) {
         if(!container.classList.contains("locked")) {
             container.innerHTML = html;
             container.style.minHeight = "fit-content"
+            setTimeout(()=>{
+              newHeight = container.offsetHeight;
+              console.log(`New height is: ${newHeight}`)
+            },100);
             if(lockAfter) {
                 setTimeout(()=>{
                     lock();
@@ -156,6 +205,7 @@ function getTinyTag(nStatus,count) {
   return ``;
 }
 function lock() {
+  console.log(`newHeight: ${newHeight}`)
     //console.log(`Calling lock method..`);
     const container = document.getElementById('guild-tags');
     if(container.classList.contains("locked")) return;
@@ -167,8 +217,11 @@ function lock() {
     ov.className = document.getElementById('darkmode').checked ? 'dark-overlay' : 'light-overlay';
     ov.innerHTML = `<img src="https://raw.githubusercontent.com/AlonsoAliaga/guild-tags/main/assets/images/lock-icon.png"><span>Click to unlock</span>`;
     container.append(ov);
-    if(typeof stored == "undefined") {
-        container.style.height = "850px";
+    if(typeof newHeight != "undefined") {
+      container.style.removeProperty('min-height');
+      container.style.height = newHeight;
+    }else if(typeof stored == "undefined") {
+      container.style.height = newHeight || "850px";
     }
     ov.onclick = function() {
         ov.remove();
@@ -177,10 +230,15 @@ function lock() {
             container.innerHTML = stored;
             container.style.minHeight = "fit-content"
             setTimeout(()=>{
+              console.log(container)
+              newHeight = container.offsetHeight;
+              console.log(`New height is: ${newHeight}`)
+            },100);
+            setTimeout(()=>{
                 lock();
             },1000 * 15);
         }else{
-            container.innerHTML = `<img src="https://raw.githubusercontent.com/AlonsoAliaga/guild-tags/main/assets/images/loading.gif" alt="Loading">
+            container.innerHTML = `<img width="120px" src="https://raw.githubusercontent.com/AlonsoAliaga/guild-tags/main/assets/images/loading.gif" alt="Loading">
                 <br><br><br><br>
                 <h2 style="font-size: 25px; font-family: MinecraftBold;">Please wait. Loading guild tags..</h2>`
             check(true);
